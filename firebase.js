2773c93b4467d5da0d6f8270349ce3d67a04624f
Kushal Neupane <mkushal10@gmail.com>
2020-08-09 17:44:05 +0545

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBz52LZD-Ax0vcA2UwbgtSbccRxSgDAukY",
+    authDomain: "tik-tok-4e811.firebaseapp.com",
+    databaseURL: "https://tik-tok-4e811.firebaseio.com",
+    projectId: "tik-tok-4e811",
+    storageBucket: "tik-tok-4e811.appspot.com",
+    messagingSenderId: "124560011294",
+    appId: "1:124560011294:web:6bae7ede7da0e597a63e0f",
+    measurementId: "G-THE35JBMEH"
+  };
