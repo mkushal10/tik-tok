118606636985685a03a9d24a51d1205b77425b03
Kushal Neupane <mkushal10@gmail.com>
2020-08-11 12:02:27 +0545

    second commit

---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+      <div className="video">
+          <h1>This is a Video</h1>
+          {/* <VideoFooter /> */}
+          {/* <VideoSidebar /> */}
+      </div>
+  )
+}
+
+export default Video;
